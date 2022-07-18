2549ac03313fbdb9bad49b76344a20bc7fa259f2
RubaCodes <rubacodes@gmail.com>
2022-07-18 17:27:22 +0200

    gestione roytre any* e view base guest

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::middleware('auth')
         Route::get('/home', 'HomeController@index')->name('home');
     });
 
+Route::get('{any?}',function(){
+    return view('guest.home');
+})->where('any','.*');
