fc19715b1837e670b8daf3a7d4008ea5cc60b4a4
RubaCodes <rubacodes@gmail.com>
2022-07-19 17:03:29 +0200

    refactor show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,16 +5,16 @@
 <div class="container margin-auto">
     <div class="card">
         <div class="card-header text-uppercase ">
-            <h1>titolo del post:</h1>
+            <h1 class="text-primary">titolo del post:</h1>
             <h2>{{$post->title}}</h2>
         </div>
         <div class="card-body">
             <div>
-                <h2>Contenuto del post</h2>
+                <h2 class="text-primary">Contenuto del post</h2>
                 {{$post->content}}
             </div>
             <div class="py-4">
-                <h2>Stato pubblicazione:</h2>
+                <h2 class="text-primary">Stato pubblicazione:</h2>
                 {{$post->published ? "Il post e' stato pubblicato" : "Il post non e' stato ancora pubblicato"}}
                 il giorno: {{$post->created_at}}
             </div>
