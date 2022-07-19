d33aa26246d1d6523f6f2467de3d6d927f27c9d0
RubaCodes <rubacodes@gmail.com>
2022-07-19 18:02:19 +0200

    Inseritto error handling su Blade

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -11,13 +11,19 @@
                 @method('PUT')
                 <div class="form-group">
                     <label for="title">Titolo del post</label>
-                    <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp"
-                        value="{{old('title', $post->title)}}">
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                        aria-describedby="emailHelp" value="{{old('title', $post->title)}}">
                 </div>
+                @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
 
                 <label for="content">Inserisci il contenuto del post</label>
-                <textarea class="form-control" id="content" name="content"
+                <textarea class="form-control @error('content') is-invalid @enderror" id="content" name="content"
                     rows="7">{{old('content',$post->content)}}</textarea>
+                @error('content')
+                <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
 
                 <div class="form-group form-check pt-4">
                     <input type="checkbox" name="published" class="form-check-input" id="published" {{old('published',
